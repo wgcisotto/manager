735e0949c32cfef9109bd8aa8776cf61c6588cbd
willgc <williamgalindoc@hotmail.com>
2015-07-24 18:39:19 -0300

    implementacao de metodo para listar os produtos

--- manager/src/br/com/wgengenharia/manager/coffe/dao/ProductDAO.java
+++ manager/src/br/com/wgengenharia/manager/coffe/dao/ProductDAO.java
@@ -1,8 +1,12 @@
 package br.com.wgengenharia.manager.coffe.dao;
 
+import java.util.List;
+
 import br.com.wgengenharia.manager.coffe.dao.model.DAOInterface;
 import br.com.wgengenharia.manager.coffe.model.Product;
 
 public interface ProductDAO extends DAOInterface<Product, Integer> {
+	
+	public List<Product> listProducts();
 
 }
--- manager/src/br/com/wgengenharia/manager/coffe/dao/ProductDAOImpl.java
+++ manager/src/br/com/wgengenharia/manager/coffe/dao/ProductDAOImpl.java
@@ -1,6 +1,9 @@
 package br.com.wgengenharia.manager.coffe.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
 import br.com.wgengenharia.manager.coffe.dao.model.DAOImpl;
 import br.com.wgengenharia.manager.coffe.model.Product;
@@ -10,6 +13,12 @@ public class ProductDAOImpl extends DAOImpl<Product, Integer> implements Product
 
 	public ProductDAOImpl(EntityManager entityManager) {
 		super(entityManager);
+	}
+
+	@Override
+	public List<Product> listProducts() {
+		TypedQuery<Product> query = em.createQuery("from Product", Product.class);
+    return query.getResultList();
 	}
 	
 
