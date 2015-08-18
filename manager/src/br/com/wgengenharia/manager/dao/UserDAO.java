25eab4b180b94e55f4453e6c17527dd5d7da04fe
willgc <williamgalindoc@hotmail.com>
2015-08-18 18:04:42 -0300

    removido user docesbean e loginbean

--- manager/src/br/com/wgengenharia/manager/dao/UserDAO.java
+++
@@ -1,8 +0,0 @@
-package br.com.wgengenharia.manager.dao;
-
-import br.com.wgengenharia.manager.dao.model.DAOInterface;
-import br.com.wgengenharia.manager.model.User;
-
-public interface UserDAO extends DAOInterface<User, Integer> {
-
-}
