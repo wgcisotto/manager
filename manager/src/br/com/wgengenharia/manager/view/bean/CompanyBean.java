4044b44bccb22583295f8acb70fac69403277422
willgc <williamgalindoc@hotmail.com>
2015-08-28 19:04:00 -0300

    novos metodos para inserir branch na company

--- manager/src/br/com/wgengenharia/manager/view/bean/CompanyBean.java
+++ manager/src/br/com/wgengenharia/manager/view/bean/CompanyBean.java
@@ -11,10 +11,12 @@ import javax.persistence.EntityManager;
 
 import org.primefaces.event.SelectEvent;
 
+import br.com.wgengenharia.manager.business.BranchBO;
 import br.com.wgengenharia.manager.business.EmployeeBO;
 import br.com.wgengenharia.manager.business.ProfileBO;
 import br.com.wgengenharia.manager.db.EntityManagerFactorySingleton;
 import br.com.wgengenharia.manager.model.Address;
+import br.com.wgengenharia.manager.model.Branch;
 import br.com.wgengenharia.manager.model.Employee;
 import br.com.wgengenharia.manager.model.Profile;
 import br.com.wgengenharia.manager.seguranca.bean.AuthenticationBean;
@@ -33,11 +35,18 @@ public class CompanyBean implements Serializable {
 	private List<Employee> employees;
 	private List<Employee> filteredEmployees;
 	private String globalFilterEmployee;
-	//Employees Address
-	private Address newAddress;
 	//Employees Profile	
 	private String profileName;
 	
+	
+	
+	//BRANCHS
+	private BranchBO branchBO;
+	private Branch newBranch;
+	private Branch selectedBranch;
+	private List<Branch> branchs;
+	private List<Branch> filteredBranchs;
+	private String globalFilterBranch;
 	
 	//DEFAULT
 	public EntityManager em;
@@ -54,14 +63,19 @@ public class CompanyBean implements Serializable {
 			
 			employeeBO = new EmployeeBO(em);
 			profileBO = new ProfileBO(em);
+			branchBO = new BranchBO(em);
 			
 			employees = employeeBO.findByCompany(userInfo.getEmployee().getCompany());
+			branchs = branchBO.findByCompany(userInfo.getEmployee().getCompany());
+			
 			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
+	
+	//EMPLOYEE
 	
 	public void newEmployee(){
 		this.newEmployee = new Employee();
@@ -71,7 +85,6 @@ public class CompanyBean implements Serializable {
 		try {
 			Profile profile = profileBO.findByName(profileName);
 			newEmployee.setProfile(profile);
-			newEmployee.setAddress(newAddress);
 			profileName = "";
 			employeeBO.insert(newEmployee);
 //		employees = employeeBO.listEmployees();// FAZER PERQUISA POR NOME DA EMPRESA E PELO BRANCH  ???
@@ -100,7 +113,60 @@ public class CompanyBean implements Serializable {
 	public void onRowSelectEmployee(SelectEvent event) {
 		this.selectedEmployee = (Employee) event.getObject();
 	}
-		
+
+	//BRANCH
+	
+	public void newBranch(){
+		this.newBranch = new Branch();
+	}
+	
+	public void addBranch(){
+		try {
+			newBranch.setCompany(userInfo.getEmployee().getCompany());
+			branchBO.insert(newBranch);
+			branchs = branchBO.findByCompany(userInfo.getEmployee().getCompany());
+			newBranch = new Branch();
+			FacesContext.getCurrentInstance().addMessage("formManager:msgBranch", new FacesMessage(FacesMessage.SEVERITY_INFO, "Sucesso!", "Filial Inserido com sucesso"));
+		} catch (Exception e) {
+			FacesContext.getCurrentInstance().addMessage("formManager:msgBranch", new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error!", e.getMessage() + " " + e.getCause()));
+		}
+	}
+	
+	public void delBranch(){
+		try {
+			if(selectedBranch!=null){
+				branchBO.delete(selectedBranch);
+				branchs = branchBO.findByCompany(userInfo.getEmployee().getCompany());
+				selectedBranch = null;
+				FacesContext.getCurrentInstance().addMessage("formManager:msgBranch", new FacesMessage(FacesMessage.SEVERITY_INFO, "Sucesso!", "Filial Excluido com sucesso"));
+			}else{
+				FacesContext.getCurrentInstance().addMessage("formManager:msgBranch", new FacesMessage(FacesMessage.SEVERITY_ERROR, "Alerta!", "Nescessário selecionar uma Filial"));
+			}
+		} catch (Exception e) {
+			FacesContext.getCurrentInstance().addMessage("formManager:msgBranch", new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error!", e.getMessage() + " " + e.getCause()));
+		}
+	}
+  
+	public void onRowSelectBranch(SelectEvent event) {
+		this.selectedBranch = (Branch) event.getObject();
+	}
+	
+	public void updateBranch(){
+		try {
+			if(selectedBranch!=null){
+				branchBO.update(selectedBranch);
+				branchs = branchBO.findByCompany(userInfo.getEmployee().getCompany());
+				selectedBranch = null;
+				FacesContext.getCurrentInstance().addMessage("formManager:msgBranch", new FacesMessage(FacesMessage.SEVERITY_INFO, "Sucesso!", "Filial Atualizada com sucesso"));
+			}else{
+				FacesContext.getCurrentInstance().addMessage("formManager:msgBranch", new FacesMessage(FacesMessage.SEVERITY_ERROR, "Alerta!", "Nescessário selecionar uma Filial"));
+			}
+		} catch (Exception e) {
+			FacesContext.getCurrentInstance().addMessage("formManager:msgBranch", new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error!", e.getMessage() + " " + e.getCause()));
+		}
+	}
+	
+	
 	
 	//get and setters
 	//EMPLOYEES
@@ -134,16 +200,44 @@ public class CompanyBean implements Serializable {
 	public void setGlobalFilterEmployee(String globalFilterEmployee) {
 		this.globalFilterEmployee = globalFilterEmployee;
 	}
-	public Address getNewAddress() {
-		return newAddress;
-	}
-	public void setNewAddress(Address newAddress) {
-		this.newAddress = newAddress;
-	}
 	public String getProfileName() {
 		return profileName;
 	}
 	public void setProfileName(String profileName) {
 		this.profileName = profileName;
 	}
+	
+	//get and setters
+	//BRANCH
+	public Branch getNewBranch() {
+		return newBranch;
+	}
+	public void setNewBranch(Branch newBranch) {
+		this.newBranch = newBranch;
+	}
+	public Branch getSelectedBranch() {
+		return selectedBranch;
+	}
+	public void setSelectedBranch(Branch selectedBranch) {
+		this.selectedBranch = selectedBranch;
+	}
+	public List<Branch> getBranchs() {
+		return branchs;
+	}
+	public void setBranchs(List<Branch> branchs) {
+		this.branchs = branchs;
+	}
+	public List<Branch> getFilteredBranchs() {
+		return filteredBranchs;
+	}
+	public void setFilteredBranchs(List<Branch> filteredBranchs) {
+		this.filteredBranchs = filteredBranchs;
+	}
+	public String getGlobalFilterBranch() {
+		return globalFilterBranch;
+	}
+	public void setGlobalFilterBranch(String globalFilterBranch) {
+		this.globalFilterBranch = globalFilterBranch;
+	}
+	
 }
