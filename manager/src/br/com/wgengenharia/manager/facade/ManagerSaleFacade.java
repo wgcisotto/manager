ae7964cf52659c35a9a279f203df8e741a3859fc
William <William@William-PC>
2015-09-16 15:07:25 -0300

    alterado facade para pegar instace do entity manager

--- manager/src/br/com/wgengenharia/manager/facade/ManagerSaleFacade.java
+++ manager/src/br/com/wgengenharia/manager/facade/ManagerSaleFacade.java
@@ -3,6 +3,7 @@ package br.com.wgengenharia.manager.facade;
 import javax.persistence.EntityManager;
 
 import br.com.wgengenharia.manager.business.SaleBO;
+import br.com.wgengenharia.manager.db.EntityManagerFactorySingleton;
 import br.com.wgengenharia.manager.model.Sale;
 
 public class ManagerSaleFacade implements ManagerSaleFacadeInterface {
@@ -10,9 +11,9 @@ public class ManagerSaleFacade implements ManagerSaleFacadeInterface {
 	private Sale sale;
 	private EntityManager em;
 	
-	public ManagerSaleFacade(Sale sale, EntityManager em) {
+	public ManagerSaleFacade(Sale sale) {
 		this.sale =  sale;
-		this.em = em;
+		this.em = EntityManagerFactorySingleton.getInstance().createEntityManager();
 	}
 	
 	
