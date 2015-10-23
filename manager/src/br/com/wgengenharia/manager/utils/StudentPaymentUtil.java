dbb12dfd00714788a29be6fafad4f5253cdfbba0
willgc <williamgalindoc@hotmail.com>
2015-10-23 18:47:40 -0200

    geracao de boleto

--- manager/src/br/com/wgengenharia/manager/utils/StudentPaymentUtil.java
+++ manager/src/br/com/wgengenharia/manager/utils/StudentPaymentUtil.java
@@ -3,26 +3,29 @@ package br.com.wgengenharia.manager.utils;
 import br.com.wgengenharia.manager.model.StudentPayments;
 
 public class StudentPaymentUtil {
+	
+	
+	private static final String ZERO_FILL = "%04d";
+	private static final String COUNTRY_CODE = "789";
+	
 
 	public static String generateStudentPaymentBarcode(StudentPayments studentPayment){
-		String branchCode = "";
-		String studentPaymentCode = "";
-		
-		for (int i = String.valueOf(studentPayment.getBranch().getId_branch()).length(); i < 4; i++) {
-			branchCode += "0";
-		}
-
-		branchCode += String.valueOf(studentPayment.getBranch().getId_branch());
-		
-		for (int i = String.valueOf(studentPayment.getId_student_payments()).length(); i < 4; i++) {
-			studentPaymentCode += "0";
-		}
-		
-		studentPaymentCode += String.valueOf(studentPayment.getId_student_payments());
-		
-		
-		return (branchCode + studentPaymentCode);
-		
+		return COUNTRY_CODE+String.format(ZERO_FILL,studentPayment.getId_student_payments())+codeResult(COUNTRY_CODE + String.format(ZERO_FILL,studentPayment.getId_student_payments()));
 	}
 	
+	public static String codeResult(String code){
+		String[] a = code.split("(?!^)");
+		int par = 0;
+		int impar = 0;
+		int result_code = 0;
+		
+		par = Integer.parseInt(a[1]) + Integer.parseInt(a[3]) + Integer.parseInt(a[5]);
+		impar = Integer.parseInt(a[0]) + Integer.parseInt(a[2]) + Integer.parseInt(a[4]) + Integer.parseInt(a[6]);
+		
+		result_code = impar * 3 + par;
+		
+		result_code = 10 - (result_code % 10);
+		 
+		return String.valueOf(result_code);
+	}
 }
